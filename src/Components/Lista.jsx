375138550549616db10fbb482afa5462593b30df
unknown <DH@DH-CARRERA-34.DH.digitalhouse.com>
2023-10-31 19:19:33 -0300

    Clase 7 - Repaso parcial

---
+++ src/Components/Lista.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Lista = ({doctores}) => {
+  return (
+    <div>
+        <ul>
+            {doctores.map((doctor) => <li key={doctor.id}>{doctor.id} - {doctor.nombre} - {doctor.especialidad}</li>)}
+        </ul>
+    </div>
+  )
+}
+
+export default Lista
